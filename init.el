1cfaf23cf05c85d483871262746e350603d4c867
Jorge Vergés <jorge.verges.contractor@bbva.com>
2024-06-28 19:52:41 +0200

    remove use-package for the great good

--- init.el
+++ init.el
@@ -3,75 +3,63 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 (package-refresh-contents)
 
-(package-install 'use-package)
 
-(eval-when-compile
-  (require 'use-package))
-
+;; INITIAL VARIABLES
 (setq is-home-station (if (getenv "HOMESTATION") t nil))
-
-(require 'use-package-ensure)
-(setq use-package-always-ensure t)
+(setq lsp-folder (getenv "LSP_FOLDER"))
 
 
+;; GENERAL PACKAGES
+;;;; EVIL
 (setq evil-want-keybinding 'nil)
-(use-package evil
-	     :bind (
-		    :map evil-insert-state-map
-		    ("C-l" . 'evil-force-normal-state))
-	     :config
-	     (evil-mode 1))
-
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init)
-  (setq evil-collection-mode-list '(mu4e)))
-
-(use-package magit
-  :bind (("C-c m k l" . magit-smerge-keep-lower)
-	 ("C-c m k u" . magit-smerge-keep-upper)))
+(if (package-installed-p 'evil) (require 'evil) (package-vc-install 'evil))
+;;(define-key evil-insert-state-map "C-l" 'evil-force-normal-state)
+(keymap-global-set "C-l" 'evil-force-normal-state)
+(evil-mode 1)
 
 
-(use-package projectile
-  :bind ("C-c p" . projectile-command-map)
-  :config
-  (projectile-mode 1))
+;;;; MAGIT
+(if (package-installed-p 'magit) (require 'magit) (package-vc-install 'magit))
+(keymap-global-set "C-c m k l" 'magit-smerge-keep-lower)
+(keymap-global-set "C-c m k u" 'magit-smerge-keep-upper)
 
-(use-package company
-  :demand t
-  :config
-  (global-company-mode))
 
-(use-package ivy
-  :config
-  (ivy-mode 1))
+;;;; PROJECTILE
+(if (package-installed-p 'projectile) (require 'projectile) (package-vc-install 'projectile))
+(keymap-global-set "C-c p" 'projectile-command-map)
+(projectile-mode 1)
 
-(global-auto-revert-mode t)
 
-(global-display-line-numbers-mode t)
-(set-face-foreground 'line-number "grey")
+;;;; COMPANY
+(if (package-installed-p 'company) (require 'company) (package-vc-install 'company))
+(global-company-mode 1)
 
-(show-paren-mode 1)
-(setq show-paren-delay 0)
 
-;; Org :)
-(use-package org-roam-ui)
-(use-package org-roam
-  :init
-  (setq org-roam-directory (file-truename "~/hive-mind"))
-  (setq org-roam-dailies-directory "daily/")
-  (setq org-roam-dailies-capture-templates
+;;;; IVY
+(if (package-installed-p 'ivy) (require 'ivy) (package-vc-install 'ivy))
+(ivy-mode 1)
+
+
+;;;; ENVRC
+(if (package-installed-p 'envrc) (require 'envrc) (package-vc-install 'envrc))
+(add-hook 'after-init-hook 'envrc-global-mode)
+
+
+;;;; ORG
+(setq org-roam-directory (file-truename "~/hive-mind"))
+(setq org-roam-dailies-directory "daily/")
+(setq org-roam-dailies-capture-templates
 	'(("d" "default" entry
            "* %?"
            :target (file+head "%<%Y-%m-%d>.org"
                               "#+title: %<%Y-%m-%d>\n"))))
-  (setq org-roam-capture-templates
+(setq org-roam-capture-templates
 	'
 	(
 	 ("d" "default" plain "%?" :target
@@ -89,107 +77,103 @@ Tables ->
 	  :unnarrowed t)
 	 )
 	)
-  (org-roam-db-autosync-mode)
-
-  :bind (("C-c o r i" . org-roam-node-insert)
-	 ("C-c o r f" . org-roam-node-find)
-	 ("C-c o r c" . org-roam-dailies-goto-today)))
+(org-roam-db-autosync-mode)
 
 
+(if (package-installed-p 'org-roam-ui) (require 'org-roam-ui) (package-vc-install 'org-roam-ui))
+(if (package-installed-p 'org-roam) (require 'org-roam) (package-vc-install 'org-roam))
 
-(add-hook 'prog-mode-hook 'flymake-mode)
-
-
-;; Erlang
-(use-package erlang)
-;; Elixir
-(use-package elixir-mode)
-;; Scala
-(use-package scala-mode)
-;; Python
-(use-package python-mode)
-(use-package pip-requirements)
-(use-package py-autopep8
-  :hook ((python-mode) . py-autopep8-mode))
+(keymap-global-set "C-c o r i" 'org-roam-node-insert)
+(keymap-global-set "C-c o r f" 'org-roam-node-find)
+(keymap-global-set "C-c o r c" 'org-roam-dailies-goto-today)
 
 
 
-;; Emacs LSP
-(use-package eglot
-  :demand t
-  :init (setq lsp-folder (getenv "LSP_FOLDER"))
-  :bind (:map evil-normal-state-map
-	      ("C-e r" . 'eglot-rename)
-	      ("C-e h" . 'eldoc)
-	      ("C-e g d" . 'xref-find-definitions))
-  :config
-  (setq eglot-ignored-server-capabilities `(:documentHighlightProvider))
-  (add-to-list 'eglot-server-programs `(erlang-mode  , (concat lsp-folder "/" "elp")))
-  (add-to-list 'eglot-server-programs `(elixir-mode , (file-symlink-p (concat lsp-folder "/" "start_lexical.sh"))))
-  (add-to-list 'eglot-server-programs `(scala-mode , (concat lsp-folder "/" "metals"))))
-  ;;(add-to-list 'eglot-server-programs `(python-mode ,(concat lsp-folder "/" "pyright") "--stdio")))
+;; DEVELOPMENT
 
-;; Emacs DAP
-(use-package dap-mode
-  :config
-  (dap-ui-mode 1)
-  ;; enables mouse hover support
-  (dap-tooltip-mode 1)
-  ;; use tooltips for mouse hover
-  ;; if it is not enabled `dap-mode' will use the minibuffer.
-  (tooltip-mode 1)
-  ;; displays floating panel with debug buttons
-  ;; requies emacs 26+
-  (dap-ui-controls-mode 1)
+;;;; Erlang
+(if (package-installed-p 'erlang) (require 'erlang) (package-vc-install 'erlang))
+;;;; Elixir
+(if (package-installed-p 'elixir-mode) (require 'elixir-mode) (package-vc-install 'elixir-mode))
+;;;; Scala
+(if (package-installed-p 'scala-mode) (require 'scala-mode) (package-vc-install 'scala-mode))
+;;;; Python
+(if (package-installed-p 'python-mode) (require 'python-mode) (package-vc-install 'python-mode))
+(if (package-installed-p 'pip-requirements) (require 'pip-requirements) (package-vc-install 'pip-requirements))
+(if (package-installed-p 'py-autopep8) (require 'py-autopep8) (package-vc-install 'py-autopep8))
+(add-hook 'python-mode-hook 'py-autopep8-mode)
+(unintern 'py-comment-region)
+(unintern 'py-uncomment)
 
-  ;; Lenguages
-  ;; Erlang
-  (require 'dap-erlang)
-  ;; Elixir
-  (require 'dap-elixir)
-  ;; Python
-  (require 'dap-python)
-  (setq dap-python-debugger 'debugpy)
-  )
+;;;; LSP -> EGLOT
+(require 'eglot)
+(setq eglot-ignored-server-capabilities `(:documentHighlightProvider))
 
+(add-to-list 'eglot-server-programs `(erlang-mode  , (concat lsp-folder "/" "elp")))
+(add-to-list 'eglot-server-programs `(elixir-mode , (file-symlink-p (concat lsp-folder "/" "start_lexical.sh"))))
+(add-to-list 'eglot-server-programs `(scala-mode , (concat lsp-folder "/" "metals")))
 
+(keymap-global-set "C-c e r" 'eglot-rename)
+(keymap-global-set "C-c e h" 'eldoc)
+(keymap-global-set "C-c e d" 'xref-find-definitions)
 
-;; Emacs COPILOT
+;;;; COPILOT
 (if is-home-station
-   (use-package gptel
-     :config
+    (
+     (if (package-installed-p 'gptel) (require 'gptel) (package-vc-install 'gptel))
      (setq
       gptel-model "codellama:latest"
       gptel-backend (gptel-make-ollama "Ollama"
                       :host "localhost:11434"
                       :stream t
                       :models '("codellama:latest")))
-     :bind (("C-c c o s" . 'gptel-send)
-	    ("C-c c o m" . 'gptel-menu))
-     )
-  )
+ 
+     (keymap-global-set "C-c c o s" 'gptel-send)
+     (keymap-global-set "C-c c o m" 'gptel-menu)))
+
+;;;; DAP
+(if (package-installed-p 'dap-mode) (require 'dap-mode) (package-vc-install 'dap-mode))
+(dap-ui-mode 1)
+(dap-tooltip-mode 1)
+(tooltip-mode 1)
+(dap-ui-controls-mode 1)
+
+(require 'dap-erlang)
+(require 'dap-elixir)
+(require 'dap-python)
+(setq dap-python-debugger 'debugpy)
 
 
-(use-package envrc
-  :config
-  (envrc-global-mode))
 
-;;;; Fancy
-;; Themes
 
-;; (use-package solo-jazz-theme)
-(use-package badwolf-theme
-  :config
-  (load-theme 'badwolf t))
 
-(use-package dashboard
-  :config
-  (dashboard-setup-startup-hook))
+;; OOOOOTHER
 
+(global-auto-revert-mode t)
+
+(global-display-line-numbers-mode t)
+(set-face-foreground 'line-number "grey")
+
+(show-paren-mode 1)
+(setq show-paren-delay 0)
+
+(add-hook 'prog-mode-hook 'flymake-mode)
+
+
+
+
+;; Fancy
+;;;; Themes
+
+(if (package-installed-p 'solo-jazz-theme) (require 'solo-jazz-theme) (package-vc-install 'solo-jazz-theme))
+(if (package-installed-p 'badwolf-theme) (require 'badwolf-theme) (package-vc-install 'badwolf-theme))
+(load-theme 'badwolf t)
+
+(if (package-installed-p 'dashboard) (require 'dashboard) (package-vc-install 'dashboard))
+(dashboard-setup-startup-hook)
 
 
 
 (setq custom-file "~/.emacs.d/custom.el")
 (when (file-exists-p custom-file) (write-region "" nil custom-file))
 (load custom-file)
-
