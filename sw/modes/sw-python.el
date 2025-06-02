d1d1aaa00a2b01958fd06273347a965cd57370be
Jorge Vergés <jorge.verges.contractor@bbva.com>
2025-06-02 10:33:43 +0200

    keep autopep8 for python mode

--- sw/modes/sw-python.el
+++ sw/modes/sw-python.el
@@ -2,17 +2,24 @@
 
 (if (package-installed-p 'python-mode) (require 'python-mode) (package-vc-install 'python-mode))
 (if (package-installed-p 'pip-requirements) (require 'pip-requirements) (package-vc-install 'pip-requirements))
+
+
+(if (package-installed-p 'py-autopep8) (require 'py-autopep8) (package-vc-install 'py-autopep8))
+(unintern 'py-comment-region)
+(unintern 'py-uncomment)
 (if (package-installed-p 'ruff-format) (require 'ruff-format) (package-vc-install 'ruff-format))
-(if (package-installed-p 'highlight-indentation) (require 'highlight-indentation) (package-vc-install 'highlight-indentation))
 
 ;; (defun ruff-lint-format ()
 ;;   "Run ruff linter an formatter in a shell."
 ;;   (shell-command "ruff check --fix; ruff format"))
 
 
-(add-hook 'python-mode-hook 'ruff-format-on-save-mode)
-(add-hook 'python-mode-hook 'highlight-indentation-current-column-mode)
+(if is-home-station
+    (add-hook 'python-mode-hook 'ruff-format-on-save-mode)
+  (add-hook 'python-mode-hook 'py-autopep8-mode))
 
+(if (package-installed-p 'highlight-indentation) (require 'highlight-indentation) (package-vc-install 'highlight-indentation))
+(add-hook 'python-mode-hook 'highlight-indentation-current-column-mode)
 (set-face-background 'highlight-indentation-current-column-face "#45413b")
 
 
