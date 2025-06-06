39e98b1c3845ea2aa635e40520a6b3d7391d391f
Jorge Vergés <jorge.verges.contractor@bbva.com>
2025-06-06 10:19:07 +0200

    feat: autopep8 with reformatter

--- sw/modes/sw-python.el
+++ sw/modes/sw-python.el
@@ -4,19 +4,28 @@
 (if (package-installed-p 'pip-requirements) (require 'pip-requirements) (package-vc-install 'pip-requirements))
 
 
-(if (package-installed-p 'py-autopep8) (require 'py-autopep8) (package-vc-install 'py-autopep8))
-(unintern 'py-comment-region)
-(unintern 'py-uncomment)
+;; (if (package-installed-p 'py-autopep8) (require 'py-autopep8) (package-vc-install 'py-autopep8))
+;; (unintern 'py-comment-region)
+;; (unintern 'py-uncomment)
 (if (package-installed-p 'ruff-format) (require 'ruff-format) (package-vc-install 'ruff-format))
 
 ;; (defun ruff-lint-format ()
 ;;   "Run ruff linter an formatter in a shell."
 ;;   (shell-command "ruff check --fix; ruff format"))
 
+;;;###autoload (autoload 'python-autopep8-format-buffer "current-file" nil t)
+;;;###autoload (autoload 'python-autopep8-format-region "current-file" nil t)
+;;;###autoload (autoload 'python-autopep8-format-on-save-mode "current-file" nil t)
+(reformatter-define python-autopep8-format
+  :program "autopep8"
+  :args (list input-file)
+  )
+
+
 
 (if is-home-station
     (add-hook 'python-mode-hook 'ruff-format-on-save-mode)
-  (add-hook 'python-mode-hook 'py-autopep8-mode))
+  (add-hook 'python-mode-hook 'python-autopep8-format-on-save-mode))
 
 (if (package-installed-p 'highlight-indentation) (require 'highlight-indentation) (package-vc-install 'highlight-indentation))
 (add-hook 'python-mode-hook 'highlight-indentation-current-column-mode)
