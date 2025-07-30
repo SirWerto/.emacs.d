a01ca9118e4e5afbaedc207a85b08a7e4ba777ff
Jorge Vergés <jorge.verges.contractor@bbva.com>
2025-07-30 14:10:45 +0200

    fix issues with gptel ollama config

--- sw/modes/sw-llm.el
+++ sw/modes/sw-llm.el
@@ -2,17 +2,18 @@
 
 (if (package-installed-p 'gptel) (require 'gptel) (package-vc-install 'gptel))
 
+
+(setq main-llm-model "smollm:135m")
 (setq gptel-backend (gptel-make-ollama "Ollama"
                  :host "localhost:11434"
                  :stream t
-                 :models '(smollm:135m)))
+                 :models (list main-llm-model)))
 
-(setq gptel-model 'smollm:135m)
+(setq gptel-model main-llm-model)
 
 
-(evil-all-global-set-key (kbd "C-c l l m b") 'gptel)
-(evil-all-global-set-key (kbd "C-c l l m q s") 'gptel-send)
-(evil-all-global-set-key (kbd "C-c l l m q m") 'gptel-menu)
-
+(evil-all-global-set-key (kbd "C-c l l m") 'gptel)
+(evil-all-global-set-key (kbd "C-c l l q s") 'gptel-send)
+(evil-all-global-set-key (kbd "C-c l l q m") 'gptel-menu)
 
 (provide 'sw-llm)
